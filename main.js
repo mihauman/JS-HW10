3a6c140b220e7dae0ee80654ebd605fc5df6afb9
mihauman <mihauman1313@gmail.com>
2020-03-15 22:08:44 +0200

    1

---
+++ main.js
@@ -0,0 +1,39 @@
+const musicArr = [
+new Audio("./sounds/1.wav"),
+new Audio("./sounds/2.wav"),
+new Audio("./sounds/3.wav"),
+new Audio("./sounds/4.wav"),
+new Audio("./sounds/5.wav"),
+new Audio("./sounds/6.wav"),
+new Audio("./sounds/7.wav"),
+new Audio("./sounds/8.wav"),
+new Audio("./sounds/9.wav")];
+
+const music = document.getElementById("music");
+
+function onClickDo(note){  
+    note.pause();
+    note.currentTime = 0;
+    note.play();  
+  }
+  
+  document.onkeydown = function(e){
+    const index = 49;
+    for(let i = 0; i <= musicArr.length; i++){
+      switch(e.keyCode){
+        case index + i: onClickDo(musicArr[i]);
+        piano.children[i].classList.add("active");
+        break;
+      }
+    }
+  }
+  
+  document.onkeyup = function(e){
+    const index = 49;
+    for(let i = 0; i <= musicArr.length; i++){
+      switch(e.keyCode){
+        case index + i: piano.children[i].classList.remove("active");
+        break;
+      }
+    }
+  }

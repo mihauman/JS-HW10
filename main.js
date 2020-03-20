5ad69122c2d76bfa1ff87c75167d95467b82299e
mihauman <mihauman1313@gmail.com>
2020-03-20 14:20:42 +0200

    1.2

--- main.js
+++ main.js
@@ -9,30 +9,21 @@ new Audio("./sounds/7.wav"),
 new Audio("./sounds/8.wav"),
 new Audio("./sounds/9.wav")];
 
-const music = document.getElementById("music");
+const music = document.getElementsByClassName("music");
 
 function click(bit){  
-    bit.pause();
-    bit.currentTime = 0,5;
-    bit.play();  
-  }
-  
-  document.onkeydown = function(e){
-    const index = 49;
-    for(let i = 0; i <= musicArr.length; i++){
-      switch(e.keyCode){
-        case index + i: click(musicArr[i]);
-        drum.children[i].classList.add("active");
-        break;
-      }
+  bit.pause();
+  bit.currentTime = 0,5;
+  bit.play();  
+}
+
+document.onkeydown = function(e){
+  const index = 49;
+  for(let i = 0; i <= musicArr.length; i++){
+    switch(e.keyCode){
+      case index + i: click(musicArr[i]);
+      music.children[i].classList.add("active");
+      break;
     }
   }
-  document.onkeyup = function(e){
-    const index = 49;
-    for(let i = 0; i <= musicArr.length; i++){
-      switch(e.keyCode){
-        case index + i: drum.children[i].classList.remove("active");
-        break;
-      }
-    }
-  }
+}

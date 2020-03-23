80c0b150754e851522bb0e2ffe34f006de580c24
mihauman <mihauman1313@gmail.com>
2020-03-23 17:30:17 +0200

    1.3

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 const musicArr = [
-new Audio("./sounds/1.wav"),
-new Audio("./sounds/2.wav"),
-new Audio("./sounds/3.wav"),
-new Audio("./sounds/4.wav"),
-new Audio("./sounds/5.wav"),
-new Audio("./sounds/6.wav"),
-new Audio("./sounds/7.wav"),
-new Audio("./sounds/8.wav"),
-new Audio("./sounds/9.wav")];
+ new Audio("./sounds/1.wav"),
+ new Audio("./sounds/2.wav"),
+ new Audio("./sounds/3.wav"),
+ new Audio("./sounds/4.wav"),
+ new Audio("./sounds/5.wav"),
+ new Audio("./sounds/6.wav"),
+ new Audio("./sounds/7.wav"),
+ new Audio("./sounds/8.wav"),
+ new Audio("./sounds/9.wav")];
 
-const music = document.getElementsByClassName("music");
+const music = document.getElementById("music");
 
-function click(bit){  
+function playMusic(bit){  
   bit.pause();
-  bit.currentTime = 0,5;
+  bit.currentTime = 0;
   bit.play();  
 }
 
@@ -21,8 +21,7 @@ document.onkeydown = function(e){
   const index = 49;
   for(let i = 0; i <= musicArr.length; i++){
     switch(e.keyCode){
-      case index + i: click(musicArr[i]);
-      music.children[i].classList.add("active");
+      case index + i: playMusic(musicArr[i]);
       break;
     }
   }
